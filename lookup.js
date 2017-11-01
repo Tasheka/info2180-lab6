1e2dccd1901d83c7dc74816f04b6714cc3c6b9bf
Tasheka Martin <tasheka_m@yahoo.com>
2017-11-01 19:20:22 +0000

    Exercise 2

--- lookup.js
+++ lookup.js
@@ -17,26 +17,5 @@ window.onload = function ()
         alert($(result).text());
     }
 });
-
-
-		////	$.get("request.php", function (){
-			//$(".dictionary").html();
-			//alert("good");})
-		//	window.location.assign(
-		//	window.alert("Its ok");
-		//	$("#search").on('click', function(){
-      // var query = $("#query").dictionary();
-       //var link = "https://info2180-lab6-jasonhinds13.c9users.io/request.php?q="+query;
-       
-       /* $.ajax(link, {
-           method: 'GET',
-       }).done(function(res){
-           $("#result").html(res);
-       }).fail(function(err){
-           $("#result").html(err);
-       });*/
-      // "https://info2180-lab6-jasonhinds13.c9users.io/request.php?q="+query
-       
- 
 		};
 		}
